3d4a461f68141d61e2afdea578ead8cc30ffddcf
Li Ming <li-m@neusoft.com>
2017-06-05 16:39:04 +0800

    Add by customize setting in "list/init-local.el"

---
+++ lisp/init-local.el
@@ -0,0 +1,22 @@
+;;; ######## My Own Custom Settings >>>
+
+;; display time
+(display-time-mode t)
+(setq display-time-24hr-format t)
+
+;; modify the cursor to a vertical line
+(setq-default cursor-type 'bar)
+
+;; display line number
+(require 'linum)
+(global-linum-mode t)
+;; highlight current line
+(global-hl-line-mode t)
+
+;; set default woking directory(C-x C-f/C-x C-v)
+;;(setq default-directory "~/work/bev_tcu/code/base/18BEV_TCU")
+;;(setq command-line-default-directory "~/work/bev_tcu/code/base/18BEV_TCU")
+
+;;; ######## My Own Custom Settings <<<
+
+(provide 'init-local)
