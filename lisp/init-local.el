048e1097c9c819a267fd7e71050775e2f0f6b797
Li Ming <li-m@neusoft.com>
2017-06-05 18:39:32 +0800

    Put one space separation after linenumber.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -10,12 +10,14 @@
 ;; display line number
 (require 'linum)
 (global-linum-mode t)
+;; put one space separation
+(setq linum-format "%d ")
+;;(setq linum-format "%d \u2502 ")
 ;; highlight current line
 (global-hl-line-mode t)
 
 ;; set default woking directory(C-x C-f/C-x C-v)
 ;;(setq default-directory "~/work/bev_tcu/code/base/18BEV_TCU")
-;;(setq command-line-default-directory "~/work/bev_tcu/code/base/18BEV_TCU")
 
 ;;; ######## My Own Custom Settings <<<
 
