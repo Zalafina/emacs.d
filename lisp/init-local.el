ba0eeb16fd4d247463bfbcbc38aced8877df50d4
Melvin Li <kkkwing15@163.com>
2017-06-06 00:04:31 +0800

    Display linenumber with "nlinum"

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -7,17 +7,16 @@
 ;; modify the cursor to a vertical line
 (setq-default cursor-type 'bar)
 
-;; display line number
-(require 'linum)
-(global-linum-mode t)
-;; put one space separation
-(setq linum-format "%d ")
+;; display line number with "nlinum"
+(global-nlinum-mode 1)
+(setq nlinum-format "%d: ")
+
 ;;(setq linum-format "%d \u2502 ")
 ;; highlight current line
 (global-hl-line-mode t)
 
 ;; set default woking directory(C-x C-f/C-x C-v)
-;;(setq default-directory "~/work/bev_tcu/code/base/18BEV_TCU")
+;;(setq default-directory "~/work/")
 
 ;;; ######## My Own Custom Settings <<<
 
